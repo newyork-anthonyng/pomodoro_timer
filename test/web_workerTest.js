488d4c451e1ad9f2ea217cb3ed4c2140d8743fae
Anthony Ng <newyork.anthonyng@gmail.com>
2016-03-27 22:02:33 -0400

    Add frontend test

---
+++ test/web_workerTest.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const should = chai.should();
+
+describe('timerWebWorker', function() {
+
+  it('timedCount');
+
+  describe('setTimer()', function() {
+    let pauseTimerStub, postMessageStub;
+
+    before(function() {
+      pauseTimerStub = sinon.stub(timerWebWorker, 'pauseTimer');
+      postMessageStub = sinon.stub(window, 'postMessage');
+
+      timerWebWorker.setTimer(15);
+    });
+
+    after(function() {
+      timerWebWorker.pauseTimer.restore();
+      window.postMessage.restore();
+    });
+
+    it('should pause timer', function() {
+      pauseTimerStub.calledOnce.should.be.true;
+    });
+
+    it('should pass "set" message', function() {
+      let testData = postMessageStub.args[0][0];
+
+      testData.action.should.equal('set');
+      testData.time.should.equal(15);
+    });
+
+  });
+
+  describe('playTimer()', function() {
+    let timedCountStub, postMessageStub;
+
+    before(function() {
+      timedCountStub = sinon.stub(timerWebWorker, 'timedCount');
+      postMessageStub = sinon.stub(window, 'postMessage');
+
+      timerWebWorker.setTimer(16);
+      timerWebWorker.playTimer();
+    });
+
+    after(function() {
+      timerWebWorker.timedCount.restore();
+      window.postMessage.restore();
+    });
+
+    it('should play timedCount', function() {
+      timedCountStub.calledOnce.should.be.true;
+    });
+
+    it('should pass "play" message', function() {
+      let testData = postMessageStub.args[0][0];
+      console.log(testData);
+/*
+      testData.action.should.equal('play');
+      testData.time.should.equal(16);
+*/
+    });
+
+  });
+
+});
