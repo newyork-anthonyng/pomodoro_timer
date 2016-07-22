d395cc6529efe92505be9ae138633ba359de360d
Anthony Ng <newyork.anthonyng@gmail.com>
2016-07-22 11:46:23 -0400

    Add basic functionality for timer

--- server.js
+++ server.js
@@ -2,43 +2,44 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
 const logger = require('morgan');
 const request = require('request');
 const Utility = require('./public/utility');
 
-app.use(express.static('public'));
+app.use(express.static('dist'));
 app.use(logger('dev'));
 
 app.get('/', (req, res) => {
-  res.json({ SUCCESS: true });
+	res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.get('/giphy', (req, res) => {
-  const queryString = Utility.parseQueryString(req.originalUrl);
-  const myUrl = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&' +
-                'tag=' + queryString['keyword'];
+	const queryString = Utility.parseQueryString(req.originalUrl);
+	const myUrl = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&' +
+		'tag=' + queryString['keyword'];
 
-  request(myUrl, (error, response, body) => {
-    if(!error && response.statusCode === 200) {
-      const jsonData = JSON.parse(body)['data']['image_original_url'];
+	request(myUrl, (error, response, body) => {
+		if(!error && response.statusCode === 200) {
+			const jsonData = JSON.parse(body)['data']['image_original_url'];
 
-      if(jsonData) {
-        res.json({
-          SUCCESS: true,
-          MESSAGE: 'Enjoy your Giphy',
-          URL: jsonData
-        });
-      } else {
-        res.json({ SUCCESS: false, MESSAGE: 'No giphy found.' });
-        return false;
-      }
-    }
-  });
+			if(jsonData) {
+				res.json({
+					SUCCESS: true,
+					MESSAGE: 'Enjoy your Giphy',
+					URL: jsonData
+				});
+			} else {
+				res.json({ SUCCESS: false, MESSAGE: 'No giphy found.' });
+				return false;
+			}
+		}
+	});
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
-  const port = server.address().port;
-  console.log('Server running on ' + port);
+	const port = server.address().port;
+	console.log('Server running on ' + port);
 });
 
 module.exports = app;
--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,103 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = React.createClass({
-	render: () => {
+	getInitialState: function() {
+		return {
+			isRunning: false,
+			seconds: 1500
+		};
+	},
+
+	componentWillMount: function() {
+		this.intervals = [];
+	},
+
+	componentWillUnmount: function() {
+		this.intervals.forEach(clearInterval);
+	},
+
+	handlePlayClicked: function() {
+		if(this.state.isRunning) {
+			this.pauseTimer();
+		} else {
+			this.playTimer();
+		}
+
+		this.setState({ isRunning: !this.state.isRunning });
+	},
+
+	playTimer: function() {
+		console.log('%c playing timer', 'background-color: lightgreen;');
+		const timer = window.setInterval(function() {
+			this.setState({ seconds: this.state.seconds - 1 });
+		}.bind(this), 1000);
+
+		this.intervals.push(timer);
+	},
+
+	pauseTimer: function() {
+		console.log('%c pausing timer', 'background-color: lightpink;');
+		this.intervals.forEach(clearInterval);
+	},
+
+	handleResetClicked: function() {
+		console.log('reset clicked');
+	},
+
+	handleSettingsClicked: function() {
+		console.log('settings clicked');
+	},
+
+	render: function() {
 		return (
-			<p>Hello World</p>
+			<div>
+				<Time seconds={this.state.seconds} />
+				<Label handleClick={this.handlePlayClicked}>{this.state.isRunning ? 'Pause' : 'Play'}</Label>
+				<Label handleClick={this.handleResetClicked}>Reset</Label>
+				<Label handleClick={this.handleSettingsClicked}>Settings</Label>
+				<Footer>Pomodoro Timer</Footer>
+			</div>
+		);
+	}
+});
+
+const Time = React.createClass({
+	formatTime: function(seconds) {
+		let minutes = parseInt(seconds / 60);
+		minutes = minutes.length < 2 ? '0' + minutes : minutes;
+		seconds = seconds % 60 + '';
+		seconds = seconds.length < 2 ? '0' + seconds : seconds;
+
+		return minutes + ':' + seconds;
+	},
+
+	render: function() {
+		return (
+			<div>
+				<p>{this.formatTime(this.props.seconds)}</p>
+			</div>
+		);
+	}
+});
+
+const Label = React.createClass({
+	render: function() {
+		return (
+			<p
+				onClick={this.props.handleClick}
+			>
+				{this.props.children}
+			</p>
+		);
+	}
+});
+
+const Footer = React.createClass({
+	render: function() {
+		return (
+			<p>
+				Pomodoro Timer
+			</p>
 		);
 	}
 });
